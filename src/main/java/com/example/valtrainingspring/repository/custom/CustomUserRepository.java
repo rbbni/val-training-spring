c57ac6d307f6411744a21dd9a13345532b0d6c20
NBK450LEH3QHDGY\User <roman.nimets97@gmail.com>
2023-09-27 17:26:04 +0200

    :(

--- src/main/java/com/example/valtrainingspring/repository/custom/CustomUserRepository.java
+++ src/main/java/com/example/valtrainingspring/repository/custom/CustomUserRepository.java
@@ -5,7 +5,8 @@ import com.example.valtrainingspring.model.User;
 public interface CustomUserRepository {
 
 	public User findByMailCustom(String mail); 
-		
 	
+	//prima lettera del nome e ultima del cognome
+	public String findMailByJobLevelIfEvaluator(String jobLevel);
 
 }
--- src/main/java/com/example/valtrainingspring/repository/custom/CustomUserRepositoryImpl.java
+++ src/main/java/com/example/valtrainingspring/repository/custom/CustomUserRepositoryImpl.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.valtrainingspring.model.User;
 
+import jakarta.persistence.criteria.Expression;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 
 public class CustomUserRepositoryImpl implements CustomUserRepository {
@@ -22,6 +24,31 @@ public class CustomUserRepositoryImpl implements CustomUserRepository {
 		criteriaQuery.select(root).where(criteriaBuilder.equal(root.get("mail"), mail));
 
 		return entityManager.createQuery(criteriaQuery).getSingleResult();
+	}
+
+	@Override
+	// prima lettera del nome e ultima del cognome
+
+	public String findMailByJobLevelIfEvaluator(String jobLevel) {
+
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+		Root<User> root = criteriaQuery.from(User.class);
+
+		Predicate isEvaluator = criteriaBuilder.equal(root.get("evalutator"), 1);
+		Predicate byJobLevel = criteriaBuilder.equal(root.get("job_level"), jobLevel);
+
+		jakarta.persistence.criteria.Expression<String> penultimateLetterExpression = criteriaBuilder.substring(root.get("surname"),
+				
+				
+				root.ge,
+				
+				1);
+
+		criteriaQuery.select(root).where(isEvaluator, byJobLevel).orderBy(criteriaBuilder.asc(root.get("name")),
+				criteriaBuilder.asc(root.get("surname")));
+
+		return null;
 	}
 
 }
